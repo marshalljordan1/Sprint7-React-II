0775c18c21ebf2a11e3f1c3d52c798c69cb1a007
marshalljordan1 <jordan_marshall@live.co.uk>
2023-04-11 11:01:23 +0200

    complete ex1

--- react-II/src/App.jsx
+++ react-II/src/App.jsx
@@ -2,9 +2,49 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [webpageChecked, setWebpageChecked] = useState(false);
+  const [consultationChecked, setConsultationChecked] = useState(false);
+  const [googleAdChecked, setGoogleAdChecked] = useState(false);
 
-  return <div></div>;
+  const webpage = 500;
+  const consultation = 300;
+  const googleAd = 200;
+
+  const addWeb = (event) => {
+    setTotal(event.target.checked ? total + webpage : total - webpage);
+    setWebpageChecked(event.target.checked);
+  };
+
+  const addConsult = (event) => {
+    setTotal(
+      event.target.checked ? total + consultation : total - consultation
+    );
+    setConsultationChecked(event.target.checked);
+  };
+
+  const addGoogle = (event) => {
+    setTotal(event.target.checked ? total + googleAd : total - googleAd);
+    setGoogleAdChecked(event.target.checked);
+  };
+
+  return (
+    <div>
+      <p>What do you want to do?</p>
+      <input type="checkbox" checked={webpageChecked} onChange={addWeb} /> A web
+      page (500€)
+      <br />
+      <input
+        type="checkbox"
+        checked={consultationChecked}
+        onChange={addConsult}
+      />{" "}
+      A SEO consultancy (300€) <br />
+      <input type="checkbox" checked={googleAdChecked} onChange={addGoogle} /> A
+      Google Ads campaign (200€) <br />
+      <p>Price: {total}€</p>
+    </div>
+  );
 }
 
 export default App;
